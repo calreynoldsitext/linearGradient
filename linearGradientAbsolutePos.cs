5505372c26c2db0d29f4e2328da758cd40c9a3f6
calreynoldsitext <68069344+calreynoldsitext@users.noreply.github.com>
2020-07-14 12:00:23 -0400

    Add files via upload

---
+++ linearGradientAbsolutePos.cs
@@ -0,0 +1,60 @@
+using System;
+using System.IO;
+using iText.Kernel.Colors;
+using iText.Kernel.Colors.Gradients;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Borders;
+using iText.Layout.Element;
+using iText.Layout.Properties;
+
+namespace iText.Samples.Sandbox.Graphics
+{
+    public class LinearGradientsInLayout
+    {
+        public static readonly string DEST = "results/linearGradientPositionBased.pdf";
+
+        public static void Main(String[] args)
+        {
+            FileInfo file = new FileInfo(DEST);
+            file.Directory.Create();
+
+            new LinearGradientsInLayout().ManipulatePdf();
+        }
+
+        protected void ManipulatePdf()
+        {
+            Document doc = new Document(new PdfDocument(new PdfWriter(DEST)));
+            AddLinearGradientAsElementBackground(doc);
+            doc.Close();
+        }
+
+        private void AddLinearGradientAsElementBackground(Document doc)
+        {
+            doc.Add(new Paragraph("The \"addLinearGradientAsElementBackground\" starts here."));
+
+            AbstractLinearGradientBuilder gradientBuilder = new StrategyBasedLinearGradientBuilder()
+                    .AddColorStop(new GradientColorStop(ColorConstants.RED.GetColorValue()))
+                    .AddColorStop(new GradientColorStop(ColorConstants.GREEN.GetColorValue()))
+                    .AddColorStop(new GradientColorStop(ColorConstants.BLUE.GetColorValue()));
+            BackgroundImage backgroundImage = new BackgroundImage(gradientBuilder);
+
+            if (backgroundImage.IsBackgroundSpecified())
+            {
+                String text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, " +
+                              "sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. " +
+                              "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi " +
+                              "ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit " +
+                              "in voluptate velit esse cillum dolore eu fugiat nulla pariatur. " +
+                              "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui " +
+                              "officia deserunt mollit anim id est laborum. ";
+
+                Div div = new Div().Add(new Paragraph(text + text + text));
+                div.SetProperty(Property.BACKGROUND_IMAGE, backgroundImage);
+                doc.Add(div);
+
+            }
+        }
+    }
+}
